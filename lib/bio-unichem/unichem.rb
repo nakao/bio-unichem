ef06f785ec5684eb69948b12289402b6daa2db80
Mitsuteru Nakao <mitsuteru.nakao@gmail.com>
2013-02-01 22:58:38 +0900

    Added BioUniChem::REST::UniChem_URI

--- lib/bio-unichem/unichem.rb
+++ lib/bio-unichem/unichem.rb
@@ -1,3 +1,90 @@
-module BioUnichem
+module BioUniChem
 
+  # UniChem REST Web service API Client.
+  #
+  #  # URI
+  #  BioUniChem::REST::UniChem_URI
+  #
+  class REST
+
+    HOST_NAME = "www.ebi.ac.uk"
+    API_ROOT = "unichem/rest"
+    BASE_URL = "http://" + HOST_NAME + "/" + API_ROOT
+
+    # BioUniChem::REST::UniChem_URI
+    module UniChem_URI
+
+      def self.address(path)
+        "#{BASE_URL}/#{path}"
+      end
+
+      # Example:  http://www.ebi.ac.uk/unichem/rest/src_compound_id/CHEMBL12/1
+      # Example:  http://www.ebi.ac.uk/unichem/rest/src_compound_id/CHEMBL12/1/2
+      def self.src_compound_id(src_compound_id, src_id, to_src_id = nil)
+        path = [src_compound_id, src_id, to_src_id].compact.join("/")
+        address("src_compound_id/#{path}")
+      end
+
+      # Example:  http://www.ebi.ac.uk/unichem/rest/src_compound_id_all/CHEMBL12/1
+      # Example:  http://www.ebi.ac.uk/unichem/rest/src_compound_id_all/CHEMBL12/1/2
+      def self.src_compound_id_all(src_compound_id, src_id, to_src_id = nil)
+        path = [src_compound_id, src_id, to_src_id].compact.join("/")
+        address("src_compound_id_all/#{path}")
+      end
+
+      # Example:  http://www.ebi.ac.uk/unichem/rest/mapping/4/1
+      def self.mapping(src_id, to_src_id)
+        address("mapping/#{src_id}/#{to_src_id}")
+      end
+
+      # Example:  http://www.ebi.ac.uk/unichem/rest/inchikey/AAOVKJBEBIDNHE-UHFFFAOYSA-N
+      def self.inchikey(inchikey)
+        address("inchikey/#{inchikey}")
+      end
+
+      # Example:  http://www.ebi.ac.uk/unichem/rest/inchikey_all/AAOVKJBEBIDNHE-UHFFFAOYSA-N
+      def self.inchikey_all(inchikey)
+        address("inchikey_all/#{inchikey}")
+      end
+ 
+      # Example:  http://www.ebi.ac.uk/unichem/rest/src_ids/
+      def self.src_ids
+        address("src_ids/")
+      end
+
+      # Example:  http://www.ebi.ac.uk/unichem/rest/sources/1
+      def self.sources(src_id)
+        address("sources/#{src_id}")
+      end
+
+      # Example:  http://www.ebi.ac.uk/unichem/rest/structure/CHEMBL12/1
+      def self.structure(src_compound_id, src_id)
+        address("structure/#{src_compound_id}/#{src_id}")
+      end
+
+      # Example:  http://www.ebi.ac.uk/unichem/rest/structure_all/CHEMBL12/1
+      def self.structure_all(src_compound_id, src_id)
+        address("structure_all/#{src_compound_id}/#{src_id}")
+      end
+
+      # Example:  http://www.ebi.ac.uk/unichem/rest/src_compound_id_url/CHEMBL12/1/2
+      def self.src_compound_id_url(src_compound_id, src_id, to_src_id)
+        address("src_compound_id_url/#{src_compound_id}/#{src_id}/#{to_src_id}")
+      end
+
+      # Example:  http://www.ebi.ac.uk/unichem/rest/src_compound_id_all_obsolete/DB07699/2
+      # Example:  http://www.ebi.ac.uk/unichem/rest/src_compound_id_all_obsolete/DB07699/2/1
+      def self.src_compound_id_all_obsolete(src_compound_id, src_id, to_src_id = nil)
+        path = [src_compound_id, src_id, to_src_id].compact.join("/")
+        address("src_compound_id_all_obsolete/#{path}")
+      end
+
+      # Example:  http://www.ebi.ac.uk/unichem/rest/verbose_inchikey/HAUGRYOERYOXHX-UHFFFAOYSA-N
+      def self.verbose_inchikey(inchikey)
+        address("verbose_inchikey/#{inchikey}")
+      end
+
+      
+    end
+  end
 end
---
+++ test/test_bio-unichem-rest-client.rb
@@ -0,0 +1,99 @@
+require 'helper'
+require 'bio-unichem/unichem.rb'
+
+class TestBioUniChemURI < Test::Unit::TestCase
+  def setup
+    @obj = BioUniChem::REST::UniChem_URI
+  end
+  
+  def test_src_compound_id_2
+    assert_equal(
+      @obj.src_compound_id("CHEMBL12", "1"), 
+      "http://www.ebi.ac.uk/unichem/rest/src_compound_id/CHEMBL12/1")
+  end
+
+  def test_src_compound_id_3
+    assert_equal(
+      @obj.src_compound_id("CHEMBL12", "1", "2"), 
+      "http://www.ebi.ac.uk/unichem/rest/src_compound_id/CHEMBL12/1/2")
+  end
+
+  def test_src_compound_id_all_2
+    assert_equal(
+      @obj.src_compound_id_all("CHEMBL12", "1"),
+      "http://www.ebi.ac.uk/unichem/rest/src_compound_id_all/CHEMBL12/1")
+  end
+
+  def test_src_compound_id_all_3
+    assert_equal(
+      @obj.src_compound_id_all("CHEMBL12", "1", "2"), 
+      "http://www.ebi.ac.uk/unichem/rest/src_compound_id_all/CHEMBL12/1/2")
+  end
+
+  def test_mapping
+    assert_equal(
+      @obj.mapping("4", "1"), 
+      "http://www.ebi.ac.uk/unichem/rest/mapping/4/1")
+  end
+  
+  def test_inchikey
+    assert_equal(
+      @obj.inchikey("AAOVKJBEBIDNHE-UHFFFAOYSA-N"),
+      "http://www.ebi.ac.uk/unichem/rest/inchikey/AAOVKJBEBIDNHE-UHFFFAOYSA-N")
+  end
+  
+  def test_inchikey_all
+    assert_equal(
+      @obj.inchikey_all("AAOVKJBEBIDNHE-UHFFFAOYSA-N"),
+      "http://www.ebi.ac.uk/unichem/rest/inchikey_all/AAOVKJBEBIDNHE-UHFFFAOYSA-N")
+  end
+  
+  def test_src_ids
+    assert_equal(
+      @obj.src_ids,
+      "http://www.ebi.ac.uk/unichem/rest/src_ids/")      
+  end
+  
+  def test_sources
+    assert_equal(
+      @obj.sources("1"),
+      "http://www.ebi.ac.uk/unichem/rest/sources/1")      
+  end
+  
+  def test_structure
+    assert_equal(
+      @obj.structure("CHEMBL12", "1"),
+      "http://www.ebi.ac.uk/unichem/rest/structure/CHEMBL12/1")    
+  end
+  
+  def test_structure_all
+    assert_equal(
+      @obj.structure_all("CHEMBL12", "1"),
+      "http://www.ebi.ac.uk/unichem/rest/structure_all/CHEMBL12/1")      
+  end
+  
+  def test_src_compound_id_url
+    assert_equal(
+      @obj.src_compound_id_url("CHEMBL12", "1", "2"),
+      "http://www.ebi.ac.uk/unichem/rest/src_compound_id_url/CHEMBL12/1/2")      
+  end
+  
+  def test_src_compound_id_all_obsolete_2
+    assert_equal(
+      @obj.src_compound_id_all_obsolete("DB07699", "2"),
+      "http://www.ebi.ac.uk/unichem/rest/src_compound_id_all_obsolete/DB07699/2")
+  end
+  
+  def test_src_compound_id_all_obsolete_3
+    assert_equal(
+      @obj.src_compound_id_all_obsolete("DB07699", "2", "1"),
+      "http://www.ebi.ac.uk/unichem/rest/src_compound_id_all_obsolete/DB07699/2/1")      
+  end
+  
+  def test_verbose_inchikey
+    assert_equal(
+      @obj.verbose_inchikey("HAUGRYOERYOXHX-UHFFFAOYSA-N"),
+      "http://www.ebi.ac.uk/unichem/rest/verbose_inchikey/HAUGRYOERYOXHX-UHFFFAOYSA-N")      
+  end
+  
+end
